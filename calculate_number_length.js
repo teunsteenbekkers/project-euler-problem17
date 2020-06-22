e036d6c1e9076cb52d45cd38ce1070bad629c752
Teun Steenbekkers <teun@doetdingen.nl>
2020-06-22 22:38:51 +0200

    Added the calculation for 4-digit numbers and implemented a function to calculate the number of letters for all numbers in a range, not counting hyphens and spaces.

--- calculate_number_length.js
+++ calculate_number_length.js
@@ -10,7 +10,7 @@ const singleDigitNumberWords = [
   "nine",
 ];
 
-const wordsForNumbersBetweenNineAndTwenty = [
+const wordsForNumbersBetweenTenAndNineteen = [
   "ten",
   "eleven",
   "twelve",
@@ -35,23 +35,55 @@ const wordsForTens = [
 ];
 
 const wordForHundred = "hundred";
+const wordForThousand = "thousand";
 
 class NumberLengthCalculator {
-  constructor(number) {
+  calculateNumberOfLettersInWrittenNumber(number) {
     const numberOfDigits = `${number}`.length;
 
     let writtenNumber = "";
-    if (numberOfDigits < 2) {
-      writtenNumber = this.getWordForNumberWithOneDigit(number);
-    } else if (numberOfDigits < 3) {
-      writtenNumber = this.getWordForNumberWithTwoDigits(number);
-      // It's a number between 99 and 1000
-    } else if (numberOfDigits < 4) {
-      writtenNumber = this.getWordForNumberWithThreeDigits(number);
-    }
+    writtenNumber = this.getWordForNumber(number);
+    const writtenNumberWithoutHyphensOrWhitespace = `${writtenNumber.replace(
+      /[\s-]+/g,
+      ""
+    )}`;
+
     console.log(
-      `The number ${number} is written as '${writtenNumber}' and has ${writtenNumber.length} characters!`
+      `The number ${number} is written as '${writtenNumber}' and has ${writtenNumberWithoutHyphensOrWhitespace.length} characters!`
     );
+
+    return writtenNumberWithoutHyphensOrWhitespace.length;
+  }
+
+  calculateNumberOfLettersInWrittenNumbersBetween(start, end) {
+    let numberOfLetters = 0;
+    for (let i = start; i < end + 1; i++) {
+      numberOfLetters += numberLengthCalculator.calculateNumberOfLettersInWrittenNumber(
+        i
+      );
+    }
+    return numberOfLetters;
+  }
+
+  getWordForNumber(number) {
+    let writtenNumber = "";
+    switch (`${number}`.length) {
+      case 1:
+        writtenNumber = this.getWordForNumberWithOneDigit(number);
+        break;
+      case 2:
+        writtenNumber = this.getWordForNumberWithTwoDigits(number);
+        break;
+      case 3:
+        writtenNumber = this.getWordForNumberWithThreeDigits(number);
+        break;
+      case 4:
+        writtenNumber = this.getWordForNumberWithFourDigits(number);
+        break;
+      default:
+        throw "Getting the word for a number with this many digits is not supported!";
+    }
+    return writtenNumber;
   }
 
   getWordForNumberWithOneDigit(number) {
@@ -69,8 +101,8 @@ class NumberLengthCalculator {
 
     let writtenNumber = "";
     if (number > 9 && number < 20) {
-      writtenNumber = wordsForNumbersBetweenNineAndTwenty[number - 10];
-      // It's a number between 19 and 100
+      writtenNumber = wordsForNumbersBetweenTenAndNineteen[number - 10];
+      // It's a number between 21 and 99
     } else {
       const secondDigit = ~~`${number}`[1];
       // These are combinations of tens and single digits
@@ -78,7 +110,8 @@ class NumberLengthCalculator {
       const tenIndex = ~~`${number}`[0] - 2;
       if (secondDigit > 0) {
         const singleDigitIndex = ~~`${number}`[1] - 1;
-        writtenNumber = `${wordsForTens[tenIndex]} ${singleDigitNumberWords[singleDigitIndex]}`;
+        // Numbers between 21 and 99 always have a hyphen '-'
+        writtenNumber = `${wordsForTens[tenIndex]}-${singleDigitNumberWords[singleDigitIndex]}`;
       } else {
         writtenNumber = `${wordsForTens[tenIndex]}`;
       }
@@ -93,23 +126,37 @@ class NumberLengthCalculator {
     }
 
     let writtenNumber = "";
-    // The first word for a hundred is the word for a single digit number (eg. two hundred)
+    // The first word for a hundred is always the word for a single digit number (eg. two hundred, three hundred)
     const singleDigitIndex = ~~`${number}`[0] - 1;
-    console.log(singleDigitIndex);
     writtenNumber = `${singleDigitNumberWords[singleDigitIndex]} ${wordForHundred}`;
 
-    // If the two last digits are not two zeros, we need more words!
+    // If the two last digits are not two zeros, it means we need to append the word for a two or one digit word (we use the same logic to keep stuff DRY!)
     const lastTwoDigitsNumber = ~~`${number}`.substring(1);
     if (lastTwoDigitsNumber > 0) {
-      if (`${lastTwoDigitsNumber}`.length > 1) {
-        writtenNumber = `${writtenNumber} and ${this.getWordForNumberWithTwoDigits(
-          lastTwoDigitsNumber
-        )}`;
-      } else {
-        writtenNumber = `${writtenNumber} and ${this.getWordForNumberWithOneDigit(
-          lastTwoDigitsNumber
-        )}`;
-      }
+      writtenNumber = `${writtenNumber} and ${this.getWordForNumber(
+        lastTwoDigitsNumber
+      )}`;
+    }
+
+    return writtenNumber;
+  }
+
+  getWordForNumberWithFourDigits(number) {
+    if (`${number}`.length !== 4) {
+      throw "You specified an invalid number!";
+    }
+
+    let writtenNumber = "";
+    // The first word for a thousand is always the word for a single digit number (eg. two thousand, three thousand)
+    const singleDigitIndex = ~~`${number}`[0] - 1;
+    writtenNumber = `${singleDigitNumberWords[singleDigitIndex]} ${wordForThousand}`;
+
+    // If the three last digits are not three zeros, it means we need to append the word for a three digit, two digit or one digit word
+    const lastThreeDigitsNumber = ~~`${number}`.substring(1);
+    if (lastThreeDigitsNumber > 0) {
+      writtenNumber = `${writtenNumber}, ${this.getWordForNumber(
+        lastThreeDigitsNumber
+      )}`;
     }
 
     return writtenNumber;
